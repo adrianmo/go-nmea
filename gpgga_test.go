ba4edb2a64775dd08f8ff23de4fa8b946f3885c2
Moreno, Adrian <adrian@morenomartinez.com>
2015-07-29 11:02:12 +0200

    refactored unit test for gpgga sentences

--- gpgga_test.go
+++ gpgga_test.go
@@ -1,8 +1,9 @@
 package nmea
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type AttrTest struct {
@@ -10,57 +11,42 @@ type AttrTest struct {
 	Value     string
 }
 
-func TestGPGGA(t *testing.T) {
+func TestGPGGAGoodSentence(t *testing.T) {
 	goodMsg := "$GPGGA,034225.077,3356.4650,S,15124.5567,E,1,03,9.7,-25.0,M,21.0,M,,0000*51"
 	sentence, err := Parse(goodMsg)
 
-	if err != nil {
-		t.Fatalf("Parse error: %s", err)
-	}
+	assert.NoError(t, err, "Unexpected error parsing good sentence")
 
+	lat, _ := NewLatLong("3356.4650 S")
+	lon, _ := NewLatLong("15124.5567 E")
 	// Attributes of the parsed sentence, and their expected values.
-	attrs := []AttrTest{
-		{Attribute: "Time", Value: "034225.077"},
-		{Attribute: "FixQuality", Value: GPS},
-		{Attribute: "NumSatellites", Value: "03"},
-		{Attribute: "HDOP", Value: "9.7"},
-		{Attribute: "Altitude", Value: "-25.0"},
-		{Attribute: "Separation", Value: "21.0"},
-		{Attribute: "DGPSAge", Value: ""},
-		{Attribute: "DGPSId", Value: "0000"},
+	expected := GPGGA{
+		Sentence: Sentence{
+			Type:     "GPGGA",
+			Fields:   []string{"034225.077", "3356.4650", "S", "15124.5567", "E", "1", "03", "9.7", "-25.0", "M", "21.0", "M", "", "0000"},
+			Checksum: "51",
+			Raw:      "$GPGGA,034225.077,3356.4650,S,15124.5567,E,1,03,9.7,-25.0,M,21.0,M,,0000*51",
+		},
+		Time:          "034225.077",
+		Latitude:      lat,
+		Longitude:     lon,
+		FixQuality:    GPS,
+		NumSatellites: "03",
+		HDOP:          "9.7",
+		Altitude:      "-25.0",
+		Separation:    "21.0",
+		DGPSAge:       "",
+		DGPSId:        "0000",
 	}
 
-	s := reflect.ValueOf(sentence)
-	for _, a := range attrs {
-		v := s.FieldByName(a.Attribute).String()
-		if v != a.Value {
-			t.Errorf("%s got %s, expected %s", a.Attribute, v, a.Value)
-		}
-	}
+	assert.EqualValues(t, expected, sentence, "Sentence values do not match")
+}
 
-	sGPGGA := sentence.(GPGGA)
-	if sGPGGA.Latitude.PrintGPS() != "3356.4650" {
-		t.Errorf("Latitude got %s, expected 3356.4650", sGPGGA.Latitude.PrintGPS())
-	}
-	if sGPGGA.Longitude.PrintGPS() != "15124.5567" {
-		t.Errorf("Longitude got %s, expected 15124.5567", sGPGGA.Longitude.PrintGPS())
-	}
-
+func TestGPGGABadFixQuality(t *testing.T) {
 	// Make sure bad fix mode is detected.
 	badMode := "$GPGGA,034225.077,3356.4650,S,15124.5567,E,5,03,9.7,-25.0,M,21.0,M,,0000*55"
-	expErr := "Invalid fix quality [5]"
-	sentence, err = Parse(badMode)
-	if err == nil || err.Error() != expErr {
-		t.Errorf("Selection mode: Got '%v', expected error '%s'", err, expErr)
-	}
+	_, err := Parse(badMode)
 
-	// Test generic parse.
-	result, err := Parse(goodMsg)
-	if err != nil {
-		t.Errorf("Parse error: %v", err)
-	}
-	if _, ok := result.(GPGGA); !ok {
-		t.Errorf("Returned type is not GPGGA")
-	}
-
+	assert.Error(t, err, "Parse error not returned")
+	assert.Equal(t, err.Error(), "Invalid fix quality [5]", "Error message not as expected")
 }
