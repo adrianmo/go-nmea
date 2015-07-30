f4636554f3dc51dc7ecb865f383a12f98c42b19d
Moreno, Adrian <adrian@morenomartinez.com>
2015-07-30 12:35:44 +0200

    refactored gpgsa unit testing

--- gpgsa_test.go
+++ gpgsa_test.go
@@ -1,75 +1,50 @@
 package nmea
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-type AttrTest struct {
-	Attribute string
-	Value     string
-}
-
-func TestGPGSA(t *testing.T) {
+func TestGPGSAGoodSentence(t *testing.T) {
 	goodMsg := "$GPGSA,A,3,22,19,18,27,14,03,,,,,,,3.1,2.0,2.4*36"
-	s, err := Parse(goodMsg)
+	sentence, err := Parse(goodMsg)
 
 	assert.NoError(t, err, "Unexpected error parsing good sentence")
-	assert.Equal(t, PrefixGPGSA, s.GetSentence().Type, "Prefix does not match")
-
-	sentence := s.(GPGSA)
-
-	expSV := []string{"22", "19", "18", "27", "14", "03"}
-	if !reflect.DeepEqual(expSV, sentence.SV) {
-		t.Errorf("SV got %q, expected %q", sentence.SV, expSV)
-	}
 
 	// Attributes of the parsed sentence, and their expected values.
-	attrs := []AttrTest{
-		{Attribute: "Mode", Value: Auto},
-		{Attribute: "FixType", Value: Fix3D},
-		{Attribute: "PDOP", Value: "3.1"},
-		{Attribute: "HDOP", Value: "2.0"},
-		{Attribute: "VDOP", Value: "2.4"},
+	expected := GPGSA{
+		Sentence: Sentence{
+			Type:     "GPGSA",
+			Fields:   []string{"A", "3", "22", "19", "18", "27", "14", "03", "", "", "", "", "", "", "3.1", "2.0", "2.4"},
+			Checksum: "36",
+			Raw:      "$GPGSA,A,3,22,19,18,27,14,03,,,,,,,3.1,2.0,2.4*36",
+		},
+		Mode:    Auto,
+		FixType: Fix3D,
+		PDOP:    "3.1",
+		HDOP:    "2.0",
+		VDOP:    "2.4",
+		SV:      []string{"22", "19", "18", "27", "14", "03"},
 	}
 
-	values := reflect.ValueOf(sentence)
-	for _, a := range attrs {
-		v := values.FieldByName(a.Attribute).String()
-		if v != a.Value {
-			t.Errorf("%s got %s, expected %s", a.Attribute, v, a.Value)
-		}
-	}
+	assert.EqualValues(t, expected, sentence, "Sentence values do not match")
+}
+
+func TestGPGSABadMode(t *testing.T) {
 	// Make sure bad fix mode is detected.
 	badMode := "$GPGSA,F,3,22,19,18,27,14,03,,,,,,,3.1,2.0,2.4*31"
-	expErr := "Invalid selection mode [F]"
-	s, err = Parse(badMode)
-	if err == nil || err.Error() != expErr {
-		t.Errorf("Selection mode: Got '%v', expected error '%s'", err, expErr)
-	}
+	_, err := Parse(badMode)
+
+	assert.Error(t, err, "Parse error not returned")
+	assert.Equal(t, "Invalid selection mode [F]", err.Error(), "Error message does not match")
+}
+
+func TestGPGSABadFix(t *testing.T) {
 	// Make sure bad fix type is detected.
 	badFixType := "$GPGSA,A,6,22,19,18,27,14,03,,,,,,,3.1,2.0,2.4*33"
-	expErr = "Invalid fix type [6]"
-	s, err = Parse(badFixType)
-	if err == nil || err.Error() != expErr {
-		t.Errorf("Fix type: Got '%v', expected error '%s'", err, expErr)
-	}
+	_, err := Parse(badFixType)
 
-	wrongMsg := "$GPVTG,A,3,22,19,18,27,14,03,,,,,,,3.1,2.0,2.4*26"
-	expErr = "Sentence type 'GPVTG' not implemented"
-	s, err = Parse(wrongMsg)
-	if err == nil || err.Error() != expErr {
-		t.Errorf("Sentence type: Got '%v', expected error '%s'", err, expErr)
-	}
-
-	// Test generic parse.
-	result, err := Parse(goodMsg)
-	if err != nil {
-		t.Errorf("Parse error: %v", err)
-	}
-	if _, ok := result.(GPGSA); !ok {
-		t.Errorf("Returned type is not GPGSA")
-	}
+	assert.Error(t, err, "Parse error not returned")
+	assert.Equal(t, "Invalid fix type [6]", err.Error(), "Error message does not match")
 }
