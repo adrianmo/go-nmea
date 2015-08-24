9ab82ad05e452b346cf59fe2d4915b77240e6f14
Moreno, Adrian <adrian@morenomartinez.com>
2015-08-24 13:26:58 +0200

    implemented new sentence type GPGLL

---
+++ gpgll.go
@@ -0,0 +1,60 @@
+package nmea
+
+import "fmt"
+
+const (
+	// PrefixGPGLL prefix for GPGLL sentence type
+	PrefixGPGLL = "GPGLL"
+	// ValidGLL character
+	ValidGLL = "A"
+	// InvalidGLL character
+	InvalidGLL = "V"
+)
+
+// GPGLL is Geographic Position, Latitude / Longitude and time.
+// http://aprs.gids.nl/nmea/#gll
+type GPGLL struct {
+	Sentence
+	Latitude  LatLong // Latitude
+	Longitude LatLong // Longitude
+	Time      string  // Time Stamp
+	Validity  string  // validity - A-valid
+}
+
+// NewGPGLL constructor
+func NewGPGLL(sentence Sentence) GPGLL {
+	s := new(GPGLL)
+	s.Sentence = sentence
+	return *s
+}
+
+// GetSentence getter
+func (s GPGLL) GetSentence() Sentence {
+	return s.Sentence
+}
+
+func (s *GPGLL) parse() error {
+	var err error
+
+	if s.Type != PrefixGPGLL {
+		return fmt.Errorf("%s is not a %s", s.Type, PrefixGPGLL)
+	}
+
+	s.Latitude, err = NewLatLong(fmt.Sprintf("%s %s", s.Fields[0], s.Fields[1]))
+	if err != nil {
+		return fmt.Errorf("GPGLL decode latitude error: %s", err)
+	}
+	s.Longitude, err = NewLatLong(fmt.Sprintf("%s %s", s.Fields[2], s.Fields[3]))
+	if err != nil {
+		return fmt.Errorf("GPGLL decode longitude error: %s", err)
+	}
+
+	s.Time = s.Fields[4]
+	s.Validity = s.Fields[5]
+
+	if s.Validity != ValidGLL && s.Validity != InvalidGLL {
+		return fmt.Errorf("GPGLL decode, invalid validity '%s'", s.Validity)
+	}
+
+	return nil
+}
