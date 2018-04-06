9b6f08a4e6c452d52043c8620457515a3171b60b
Ilia Choly <icholy@accipiterradar.com>
2018-04-06 17:07:01 -0400

    update gpgll tests

--- gpgll_test.go
+++ gpgll_test.go
@@ -6,33 +6,54 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGPGLLGoodSentence(t *testing.T) {
-	goodMsg := "$GPGLL,3926.7952,N,12000.5947,W,022732,A,A*58"
-	s, err := Parse(goodMsg)
-
-	assert.NoError(t, err, "Unexpected error parsing good sentence")
-	assert.Equal(t, PrefixGPGLL, s.Prefix(), "Prefix does not match")
-
-	sentence := s.(GPGLL)
-
-	assert.Equal(t, "3926.7952", sentence.Latitude.PrintGPS(), "Latitude does not match")
-	assert.Equal(t, "12000.5947", sentence.Longitude.PrintGPS(), "Longitude does not match")
-	assert.Equal(t, Time{true, 2, 27, 32, 0}, sentence.Time, "Time does not match")
-	assert.Equal(t, "A", sentence.Validity, "Status does not match")
+var gpglltests = []struct {
+	name string
+	raw  string
+	err  string
+	msg  GPGLL
+}{
+	{
+		name: "good sentence",
+		raw:  "$GPGLL,3926.7952,N,12000.5947,W,022732,A,A*58",
+		msg: GPGLL{
+			Latitude:  MustParseLatLong("3926.7952 N"),
+			Longitude: MustParseLatLong("12000.5947 W"),
+			Time: Time{
+				Valid:       true,
+				Hour:        2,
+				Minute:      27,
+				Second:      32,
+				Millisecond: 0,
+			},
+			Validity: "A",
+		},
+	},
+	{
+		name: "bad validity",
+		raw:  "$GPGLL,3926.7952,N,12000.5947,W,022732,D,A*5D",
+		err:  "nmea: GPGLL invalid validity: D",
+	},
+	{
+		name: "wrong sentence",
+		raw:  "$GPXTE,A,A,4.07,L,N*6D",
+		err:  "nmea: GPGLL invalid prefix: GPXTE",
+	},
 }
 
-func TestGPGLLBadSentence(t *testing.T) {
-	badMsg := "$GPGLL,3926.7952,N,12000.5947,W,022732,D,A*5D"
-	_, err := Parse(badMsg)
-
-	assert.Error(t, err, "Parse error not returned")
-	assert.Equal(t, "nmea: GPGLL invalid validity: D", err.Error(), "Incorrect error message")
-}
-
-func TestGPGLLWrongSentence(t *testing.T) {
-	wrongMsg := "$GPXTE,A,A,4.07,L,N*6D"
-	_, err := Parse(wrongMsg)
-
-	assert.Error(t, err, "Parse error not returned")
-	assert.Equal(t, "nmea: sentence type 'GPXTE' not implemented", err.Error(), "Incorrect error message")
+func TestGPGLL(t *testing.T) {
+	for _, tt := range gpglltests {
+		t.Run(tt.name, func(t *testing.T) {
+			sent, err := ParseSentence(tt.raw)
+			assert.NoError(t, err)
+			gpgll, err := NewGPGLL(sent)
+			if tt.err != "" {
+				assert.Error(t, err)
+				assert.EqualError(t, err, tt.err)
+			} else {
+				assert.NoError(t, err)
+				gpgll.Sent = Sent{}
+				assert.Equal(t, tt.msg, gpgll)
+			}
+		})
+	}
 }
