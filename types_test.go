f374963aee98ff68137dabd7d0f867cd1e4b9dc7
Ilia Choly <icholy@accipiterradar.com>
2018-04-09 11:37:46 -0400

    test ParseLatLong

--- types_test.go
+++ types_test.go
@@ -9,6 +9,30 @@ import (
 
 var nearDistance = 0.001
 
+func TestParseLatLong(t *testing.T) {
+	var tests = []struct {
+		value    string
+		expected LatLong
+		err      bool
+	}{
+		{"33\u00B0 12' 34.3423\"", 33.209540, false}, // dms
+		{"3345.1232 N", 33.752054, false},            // gps
+		{"151.234532", 151.234532, false},            // decimal
+	}
+	for _, tt := range tests {
+		t.Run(tt.value, func(t *testing.T) {
+			l, err := ParseLatLong(tt.value)
+			if tt.err {
+				assert.Error(t, err)
+			} else {
+				if !l.IsNear(tt.expected, nearDistance) {
+					t.Errorf("ParseLatLong got %f, expected %f", l, tt.expected)
+				}
+			}
+		})
+	}
+}
+
 func TestParseGPS(t *testing.T) {
 	var tests = []struct {
 		value    string
