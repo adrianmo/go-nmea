708147fb1427ff5f9bdf41a3a234bf7ea343e97e
Ilia Choly <icholy@accipiterradar.com>
2018-12-19 14:37:05 -0500

    combine rmc tests

--- rmc_test.go
+++ rmc_test.go
@@ -59,6 +59,39 @@ var rmctests = []struct {
 		raw:  "$GNRMC,220516,D,5133.82,N,00042.24,W,173.8,231.8,130694,004.2,W*6B",
 		err:  "nmea: RMC invalid validity: D",
 	},
+	{
+		name: "good sentence A",
+		raw:  "$GPRMC,220516,A,5133.82,N,00042.24,W,173.8,231.8,130694,004.2,W*70",
+		msg: RMC{
+			Time:      Time{true, 22, 5, 16, 0},
+			Validity:  "A",
+			Speed:     173.8,
+			Course:    231.8,
+			Date:      Date{true, 13, 6, 94},
+			Variation: -4.2,
+			Latitude:  MustParseGPS("5133.82 N"),
+			Longitude: MustParseGPS("00042.24 W"),
+		},
+	},
+	{
+		name: "good sentence B",
+		raw:  "$GPRMC,142754.0,A,4302.539570,N,07920.379823,W,0.0,,070617,0.0,E,A*3F",
+		msg: RMC{
+			Time:      Time{true, 14, 27, 54, 0},
+			Validity:  "A",
+			Speed:     0,
+			Course:    0,
+			Date:      Date{true, 7, 6, 17},
+			Variation: 0,
+			Latitude:  MustParseGPS("4302.539570 N"),
+			Longitude: MustParseGPS("07920.379823 W"),
+		},
+	},
+	{
+		name: "bad validity",
+		raw:  "$GPRMC,220516,D,5133.82,N,00042.24,W,173.8,231.8,130694,004.2,W*75",
+		err:  "nmea: RMC invalid validity: D",
+	},
 }
 
 func TestRMC(t *testing.T) {
