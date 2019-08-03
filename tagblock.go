8138e388569a221b98a1d717e516c80477016ed0
Bjarte Klyve Larsen <bjarteklarsen@gmail.com>
2019-08-03 23:02:45 +0200

    Early exit if no matches in tagblock

--- tagblock.go
+++ tagblock.go
@@ -53,59 +53,59 @@ func parseUint(raw string) (uint32, error) {
 // https://rietman.wordpress.com/2016/09/17/nemastudio-now-supports-the-nmea-0183-tag-block/
 func parseTagBlock(raw string) (TagBlock, string, error) {
 	matches := TagBlockRegexp.FindStringSubmatch(raw)
-	var tagBlock TagBlock
-	if matches != nil {
-		raw = matches[3]
-		tags := matches[2]
-		tagBlock.Head = matches[1]
-
-		sumSepIndex := strings.Index(tags, ChecksumSep)
-		if sumSepIndex == -1 {
-			return tagBlock, raw, fmt.Errorf("nmea: tagblock does not contain checksum separator")
-		}
-
-		var (
-			fieldsRaw   = tags[0:sumSepIndex]
-			checksumRaw = strings.ToUpper(tags[sumSepIndex+1:])
-			checksum    = xorChecksum(fieldsRaw)
-			err         error
-		)
-
-		// Validate the checksum
-		if checksum != checksumRaw {
-			return tagBlock, raw, fmt.Errorf("nmea: tagblock checksum mismatch [%s != %s]", checksum, checksumRaw)
-		}
-
-		data := strings.Split(tags[:sumSepIndex], ",")
-		for _, item := range data {
-			switch item[0:1] {
-			case TypeUnixTime:
-				tagBlock.Time, err = parseUint(item)
-				if err != nil {
-					return tagBlock, raw, err
-				}
-			case TypeDestinationID:
-				tagBlock.Destination = item[2:]
-			case TypeGrouping:
-				tagBlock.Grouping = item[2:]
-			case TypeLineCount:
-				tagBlock.LineCount, err = parseUint(item)
-				if err != nil {
-					return tagBlock, raw, err
-				}
-			case TypeRelativeTime:
-				tagBlock.RelativeTime, err = parseUint(item)
-				if err != nil {
-					return tagBlock, raw, err
-				}
-			case TypeSourceID:
-				tagBlock.Source = item[2:]
-			case TypeTextString:
-				tagBlock.Text = item[2:]
-			}
-		}
-
+	if matches == nil {
+		return TagBlock{}, raw, nil
 	}
 
+	tagBlock := TagBlock{}
+	raw = matches[3]
+	tags := matches[2]
+	tagBlock.Head = matches[1]
+
+	sumSepIndex := strings.Index(tags, ChecksumSep)
+	if sumSepIndex == -1 {
+		return tagBlock, raw, fmt.Errorf("nmea: tagblock does not contain checksum separator")
+	}
+
+	var (
+		fieldsRaw   = tags[0:sumSepIndex]
+		checksumRaw = strings.ToUpper(tags[sumSepIndex+1:])
+		checksum    = xorChecksum(fieldsRaw)
+		err         error
+	)
+
+	// Validate the checksum
+	if checksum != checksumRaw {
+		return tagBlock, raw, fmt.Errorf("nmea: tagblock checksum mismatch [%s != %s]", checksum, checksumRaw)
+	}
+
+	data := strings.Split(tags[:sumSepIndex], ",")
+	for _, item := range data {
+		switch item[0:1] {
+		case TypeUnixTime:
+			tagBlock.Time, err = parseUint(item)
+			if err != nil {
+				return tagBlock, raw, err
+			}
+		case TypeDestinationID:
+			tagBlock.Destination = item[2:]
+		case TypeGrouping:
+			tagBlock.Grouping = item[2:]
+		case TypeLineCount:
+			tagBlock.LineCount, err = parseUint(item)
+			if err != nil {
+				return tagBlock, raw, err
+			}
+		case TypeRelativeTime:
+			tagBlock.RelativeTime, err = parseUint(item)
+			if err != nil {
+				return tagBlock, raw, err
+			}
+		case TypeSourceID:
+			tagBlock.Source = item[2:]
+		case TypeTextString:
+			tagBlock.Text = item[2:]
+		}
+	}
 	return tagBlock, raw, nil
 }
