955e3a22ef36d6b23b3c72914d85cc566802bb99
kvartborg <hello@kvartborg.net>
2020-02-04 12:47:12 +0100

    Add VHW water speed and heading

---
+++ vhw_test.go
@@ -0,0 +1,47 @@
+package nmea
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var vhw = []struct {
+	name string
+	raw  string
+	err  string
+	msg  VHW
+}{
+	{
+		name: "good sentence",
+		raw:  "$VWVHW,45.0,T,43.0,M,3.5,N,6.4,K*56",
+		msg: VHW{
+			TrueHeading:            45.0,
+			MagneticHeading:        43.0,
+			SpeedThroughWaterKnots: 3.5,
+			SpeedThroughWaterKPH:   6.4,
+		},
+	},
+	{
+		name: "bad sentence",
+		raw:  "$VWVHW,T,45.0,43.0,M,3.5,N,6.4,K*56",
+		err:  "nmea: VWVHW invalid true heading: T",
+	},
+}
+
+func TestVHW(t *testing.T) {
+	for _, tt := range vhw {
+		t.Run(tt.name, func(t *testing.T) {
+			m, err := Parse(tt.raw)
+			if tt.err != "" {
+				assert.Error(t, err)
+				assert.EqualError(t, err, tt.err)
+			} else {
+				assert.NoError(t, err)
+				wpl := m.(VHW)
+				wpl.BaseSentence = BaseSentence{}
+				assert.Equal(t, tt.msg, wpl)
+			}
+		})
+	}
+}
